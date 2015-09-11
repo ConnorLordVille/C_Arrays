604c3d70de8c3bfd956d9bbce6e19f07fcfcad17
MeliCity <Joseph_Meli@outlook.com>
2015-09-11 17:14:23 -0400

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -25,7 +25,9 @@ void loadBookName(char file_name[],char array1[],char array2[])
 	FILE *fp;
 	// buffer for reading the text file
 	char buffer [100];
-	// opens file ffvs
+	char * token;
+	const char comma;
+	// opens file 
 	fp = fopen(file_name,"r");
 	//checks the file to make sure it contains info
 	// otherwise exits program
@@ -41,6 +43,21 @@ void loadBookName(char file_name[],char array1[],char array2[])
 	{
 		printf("%s\n",strtok(buffer, ","));
 		
+		
+/*char str[80] = "This is - www.tutorialspoint.com - website";
+   const char s[2] = "-";
+   char *token;
+   
+    get the first token 
+   token = strtok(str, s);
+   
+   walk through other tokens
+   while( token != NULL ) 
+   {
+      printf( " %s\n", token );
+    
+      token = strtok(NULL, s);
+   }*/
 	}
 	
 	
