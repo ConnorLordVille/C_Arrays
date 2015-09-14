61f49e8b9a644daa262a5434e7fe90ed0eaff986
MeliCity <Joseph_Meli@outlook.com>
2015-09-14 11:51:33 -0400

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -8,13 +8,17 @@ void loadBookName(char file_name[], char* books[],char* writers[]);
 int main(int argc,char* argv[])
 {	//will hold the file name
 	char file[100];
-
 	char *author[55];
 	char *bookname[55];
+	int x;
 	printf("File Name:");
 	scanf("%s",file);
 	loadBookName(file, author,bookname);
-	//printf("%s\n %s",author[55],bookname[55] );
+	/*for( x = 0; x < 55; x ++)
+	{
+	printf("%s\n",author[x] );
+	printf("%s\n",bookname[x]);
+}*/
 	return 0;
 }
 //Precondtion:
@@ -24,11 +28,13 @@ void loadBookName(char file_name[],char *books[],char* writers[])
 	FILE *fp;
 	// buffer for reading the text file
 	char buffer [500];
+	// char that will hold strings to save to arrays
 	char *token;
-	const char comma[2] = ",";
+	// what we will read too
+	const char comma = ',';
+	// loop control variables
 	int i = 0;
 	int j= 0;
-	int x = 0;
 	// opens file
 	fp = fopen(file_name,"r");
 	//checks the file to make sure it contains info
@@ -42,29 +48,21 @@ void loadBookName(char file_name[],char *books[],char* writers[])
 		while (fgets(buffer,500,fp)!=NULL)
 		{// this loop goes through file using the buffer
 			// reads from the begging of the line to the comma
-
-			token = strtok(buffer, comma);
-			//printf("%s\n",token );
-		//	books[i] = token;
+			printf("BT1:%s\n",token );
+			token = strtok(buffer, &comma);
+			printf("AT1:%s\n",token );
 				i++;
 			while( token != NULL )
 			{
 			// load into array
-			writers[j] = token;
-			printf("%s\n",token );
-				 token = strtok(NULL, "\n");
-
-
+				printf("BT2:%s\n",token);
+				 token = strtok(NULL,"\n");
+				 printf("AT2:%s\n",token);
+				 token = NULL;
 			}
 				j++;
-}
+}//end of while
 
-//for( x = 0; x < 55; x ++)
-//{
-//printf("%s\n",books[x] );
-//printf("%s\n",writers[x]);
-//}
-		//end of while
 	fclose(fp);
 }
 //Postcondtion:
